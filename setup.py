0225118697d207c93e0dacbda1a6910608fc92ec
John-Charles D. Sokolow <john.charles.sokolow@gmail.com>
2014-08-17 11:06:50 -0500

    On branch master added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='fs',
+    version='0.1',
+    description='A really simple wrapper on common system tasks.',
+    author='John-Charles D. Sokolow',
+    author_email='john.charles.sokolow@gmail.com',
+    url='',
+    packages=['fs']
+)
