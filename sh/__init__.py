83e7a4ce761b805ec2bf036c7f527fe1a754aad8
John-Charles D. Sokolow <john.charles.sokolow@gmail.com>
2014-08-23 10:12:07 -0500

    On branch master GRG refactor of the fs module to the sh module.

--- sh/__init__.py
+++ sh/__init__.py
@@ -118,7 +118,7 @@ def exists(*args):
     return py_exists(join(*args))
 
 
-def fs(cmd, *paths):
+def sh(cmd, *paths):
     if cmd == "join":
         return join(*paths)
     
--- test.py
+++ test.py
@@ -6,9 +6,9 @@ import os
 from getpass import getuser
 from os.path import join, exists, expanduser
 
-from fs import fs, save, load, exists as fs_exists, FSException
+from sh import sh
 
-class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
+class TestJoiningAndManipulatingPaths(unittest.TestCase):
     
     def setUp(self):        
         self.test_root = tempfile.mkdtemp()
@@ -17,37 +17,37 @@ class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
     
     def test_it_joins_two_strings(self):
         
-        path = fs('join', '/home','user')
+        path = sh('join', '/home','user')
         self.assertEqual(path, '/home/user')
 
     def test_it_doesnt_allow_multiple_slashes(self):
         
-        path = fs('join', '/home','/user')
+        path = sh('join', '/home','/user')
         self.assertEqual(path, '/home/user')
 
     def test_it_doesnt_allow_multiple_slashes_from_behind(self):
         
-        path = fs('join', '/home/', 'user')
+        path = sh('join', '/home/', 'user')
         self.assertEqual(path, '/home/user')
 
     def test_it_can_also_join_sub_lists(self):
         
-        path = fs('join', '/home', ('a', 'b'));
+        path = sh('join', '/home', ('a', 'b'));
         self.assertEqual(path, '/home/a/b')
 
     def test_it_can_join_on_tild(self):
         
-        path = fs('join', '~', 'Desktop')
+        path = sh('join', '~', 'Desktop')
         self.assertEqual(path, expanduser('~') + '/Desktop')
 
     def test_it_can_resolve_a_tild_username(self):
         
-        path = fs('join', '~' + getuser(), 'Desktop')        
+        path = sh('join', '~' + getuser(), 'Desktop')        
         self.assertEqual(path, self.user_dir + "/" + getuser() + "/Desktop")
 
     def test_it_can_expand_environment_variables(self):
         os.environ['USER'] = "TestUser"
-        path = fs('join', '/home/$USER/Desktop')
+        path = sh('join', '/home/$USER/Desktop')
 
         self.assertEqual(path, '/home/TestUser/Desktop')
 
@@ -55,19 +55,24 @@ class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
 
         os.environ['USER'] = "TestUser"
         os.environ['OTHER'] = "OtherThing"
-        path = fs('join', '/home/$USER/Desktop/$OTHER')
+        path = sh('join', '/home/$USER/Desktop/$OTHER')
 
         self.assertEqual(path, '/home/TestUser/Desktop/OtherThing')
+        
+        
+class TestBasicDirectoryOperations(unittest.TestCase):
+    
+    pass
     
         
 #   def test_it_joins_complex_paths(self):
 #       
-#       path = fs("join", "/home/user", ('dir_one', 'dir_two'), "file.txt")        
+#       path = sh("join", "/home/user", ('dir_one', 'dir_two'), "file.txt")        
 #       self.assertEqual(path, "/home/user/dir_one/dir_two/file.txt")
 #       
 #   def test_it_joins_paths_starting_in_tild(self):
 #       
-#       path = fs("join", "~/Desktop")
+#       path = sh("join", "~/Desktop")
 #       
 #       self.assertEqual(path, os.path.expanduser('~/Desktop'))
 #   
@@ -75,7 +80,7 @@ class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
 #       
 #       test_path = join(self.test_root, "1/2/3/4/5/6/7/8/9/10")
 #       
-#       fs("mkdir -p", test_path)
+#       sh("mkdir -p", test_path)
 #       
 #       self.assertTrue(exists(test_path))
 #       
@@ -83,15 +88,15 @@ class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
 #       
 #       test_path = join(self.test_root, "one/two/three/four")
 #       
-#       fs("mkdir -p", self.test_root, "one", ("two", "three"), "four")
+#       sh("mkdir -p", self.test_root, "one", ("two", "three"), "four")
 #       
 #       self.assertTrue(exists(test_path))
 #       
 #   def test_mkdirp_doesnt_fail_if_dirs_already_exist(self):        
 #       test_path = join(self.test_root, "a/b/c/d/e/f/g")
 #       
-#       fs("mkdir -p", test_path)
-#       fs("mkdir -p", test_path)
+#       sh("mkdir -p", test_path)
+#       sh("mkdir -p", test_path)
 #       
 #       self.assertTrue(exists(test_path))
 #       
@@ -99,13 +104,13 @@ class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
 #       test_path = join(self.test_root, "a1/b2/c3/d4")
 #       
 #       open(join(self.test_root, "a1"), "wb").close()
-#       self.assertRaises(FSException, lambda: fs("mkdir -p", test_path))
+#       self.assertRaises(FSException, lambda: sh("mkdir -p", test_path))
 #       
 #   def test_exists(self):
-#       test_path = fs("join", self.test_root, "n1/n2/n3/n4/n5/n6")
+#       test_path = sh("join", self.test_root, "n1/n2/n3/n4/n5/n6")
 #       
-#       fs("mkdir -p", test_path)        
-#       self.assertTrue(fs("exists", test_path))
+#       sh("mkdir -p", test_path)        
+#       self.assertTrue(sh("exists", test_path))
 #       
 #   def test_it_can_save_a_string_to_a_file(self):        
 #       test_string = "This is a test"
@@ -115,7 +120,7 @@ class Test_FS_A_Really_Simple_Wrapper(unittest.TestCase):
 #       
 #   def test_it_can_read_from_a_file_to_a_string(self):
 #       test_content = "This is some test content!"
-#       test_path = fs('join', self.test_root, 'a_file.txt')
+#       test_path = sh('join', self.test_root, 'a_file.txt')
 #       
 #       f = open(test_path, 'wb')
 #       f.write(test_content)
