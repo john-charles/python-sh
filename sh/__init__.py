2cef16d008c8df12fbb53692974dfdb548d2eeff
John-Charles D. Sokolow <john.charles.sokolow@gmail.com>
2014-08-23 13:58:59 -0500

    On branch master grg refactor of the make_dir_p function.

--- sh/__init__.py
+++ sh/__init__.py
@@ -4,41 +4,7 @@ import os, re
 
 from SHException import SHException
 from join_listlike import join_listlike
-
-from os.path import (
-    join as py_join,
-    isdir as py_isdir,
-    exists as py_exists
-)
-
-
-
-    
-    
-def make_dir_p(options, arguments):
-    
-    if 'parents' in options and options['parents']:
-        
-        whole = ""
-        
-        for part in join_listlike({"as_list": True}, arguments):
-            whole += "/%s" % part
-            
-            if not py_exists(whole):
-                os.mkdir(whole)
-            if not py_isdir(whole):
-                raise SHException("File exists at %s" % whole)
-        
-    else:
-        
-        path = join_listlike({},arguments)
-        try:
-            os.mkdir(path)
-        except Exception, e:
-            raise SHException(e.message)
-    
-    
-    
+from dir_opts import make_dir_p
         
         
 def save(*args, **kw):
